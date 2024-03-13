5838d09f1e4ed170a65802007a30abf74b0e8884
SosthenesMS <sosthenesms@gmail.com>
2024-03-13 15:28:18 -0300

    First commit

---
+++ src/main/java/com/servico/email/java/ServicoDeEmailJavaApplication.java
@@ -0,0 +1,13 @@
+package com.servico.email.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicoDeEmailJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicoDeEmailJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/servico/email/java/ServicoDeEmailJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.servico.email.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicoDeEmailJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
